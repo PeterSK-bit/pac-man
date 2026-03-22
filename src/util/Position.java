1775d339f82e3092c1a8f4e7710733328c25d3dc
Peter Magdík <petermagdik@gmail.com>
2026-03-22 10:39:37 +0100

    feat: add position class

---
+++ src/util/Position.java
@@ -0,0 +1,75 @@
+package util;
+
+import java.util.Objects;
+
+/**
+ * Immutable 2D position within the game grid.
+ */
+public class Position {
+    private final int x;
+    private final int y;
+
+    /**
+     * Creates a position with given coordinates.
+     * @param x column index (0 = leftmost)
+     * @param y row index (0 = topmost)
+     */
+    public Position(int x, int y) {
+        if  (x <= 0 || y <= 0) {
+            throw new IllegalArgumentException("x and y should not be negative");
+        }
+
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * @return x-coordinate
+     */
+    public int getX() {
+        return this.x;
+    }
+
+    /**
+     * @return y-coordinate
+     */
+    public int getY() {
+        return this.y;
+    }
+
+    /**
+     * Creates a new position translated by the given direction.
+     *
+     * @param direction movement direction
+     * @return translated position
+     */
+    public Position translate(Direction direction) {
+        return new Position(this.x + direction.dx(), this.y + direction.dy());
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == this) {
+            return true;
+        }
+
+        if (object == null || this.getClass() != object.getClass()) {
+            return false;
+        }
+
+        Position position = (Position)object;
+
+        return (this.x == position.x && this.y == position.y);
+    }
+
+    // Required for correct behavior in hash-based collections (maybe will come handy)
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.x, this.y);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d; %d]", this.x, this.y);
+    }
+}
