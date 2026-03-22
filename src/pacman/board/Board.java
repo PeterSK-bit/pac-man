ee5d44f50ec1f0c7f9779bf7d39e1bb47d94e5d6
Peter Magdík <petermagdik@gmail.com>
2026-03-22 17:46:10 +0100

    feat: incomplete Board implementation

--- src/pacman/board/Board.java
+++ src/pacman/board/Board.java
@@ -1,4 +1,24 @@
 package pacman.board;
 
+import pacman.board.cell.Cell;
+
 public class Board {
+    private Cell[][] grid;
+    private int remainingDots;
+
+    public Board() {
+
+    }
+
+    public Cell getCell(int col, int row) {
+        return this.grid[col][row];
+    }
+
+    public boolean isWalkable(int col, int row) {
+        return this.getCell(col, row).isWalkable();
+    }
+
+    public int getRemainingDots() {
+        return this.remainingDots;
+    }
 }
