5c40a0bbffd6509e6764f9cd7e6a5216724e789a
Peter Magdík <petermagdik@gmail.com>
2026-03-22 17:46:30 +0100

    feat: implement GhostState enum

--- src/pacman/util/GhostState.java
+++ src/pacman/util/GhostState.java
@@ -1,4 +1,8 @@
 package pacman.util;
 
 public enum GhostState {
+    CHASE,      // hunt player
+    SCATTER,    // run to your corner and circle there
+    FRIGHTENED, // scared fo player, running away
+    RESPAWNING; // traveling to house for respawn
 }
