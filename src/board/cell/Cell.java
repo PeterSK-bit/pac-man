4060807a26007c62f1d6c647ed62c5fa50fddf8e
Peter Magdík <petermagdik@gmail.com>
2026-03-22 10:39:52 +0100

    feat: add cell abstract class

--- src/board/cell/Cell.java
+++ src/board/cell/Cell.java
@@ -1,4 +1,40 @@
 package board.cell;
 
-public class Cell {
+import util.Position;
+import util.ScoreManager;
+
+public abstract class Cell {
+    private final int size = 32;
+
+    private Position position;
+
+    public Cell(int x, int y) {
+        this.position = new Position(x, y);
+    }
+
+    public Cell(Position position) {
+        this.position = position;
+    }
+
+    public int getY() {
+        return this.position.getY();
+    }
+
+    public int getX() {
+        return this.position.getX();
+    }
+
+    public Position getPosition() {
+        return this.position;
+    }
+
+    public abstract void draw();
+
+    public abstract void hide();
+
+    public abstract void onEnter(ScoreManager scoreManager);
+
+    public abstract boolean isWalkable();
+
+
 }
