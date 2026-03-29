be1ec8641ed541a5e1ded40e7fc298f85a159091
Peter Magdík <petermagdik@gmail.com>
2026-03-29 16:51:19 +0200

    feat: implement ScoreManager class

--- src/pacman/util/ScoreManager.java
+++ src/pacman/util/ScoreManager.java
@@ -1,4 +1,40 @@
 package pacman.util;
 
 public class ScoreManager {
+    private final int startScore;
+    private int score;
+    private final int startLives;
+    private int lives;
+
+    public ScoreManager() {
+        this.startScore = 0;
+        this.score = 0;
+        this.startLives = 0;
+        this.lives = 3;
+    }
+
+    public ScoreManager(int score, int lives) {
+        this.startScore = score;
+        this.score = score;
+        this.startLives = lives;
+        this.lives = lives;
+    }
+
+    // todo find exact values
+    public void addDotPoints() {
+        this.score++;
+    }
+
+    public void addPowerPelletPoints() {
+        this.score += 10;
+    }
+
+    public void reset()  {
+        this.score = this.startScore;
+        this.lives = this.startLives;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
 }
