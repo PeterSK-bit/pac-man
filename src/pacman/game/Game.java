235012152231c2a7e93c3b272a643bfb4f0eb756
Peter Magdík <petermagdik@gmail.com>
2026-04-05 12:26:24 +0200

    feat: add event handler skeletons for ShapesGE callbacks

--- src/pacman/game/Game.java
+++ src/pacman/game/Game.java
@@ -1,4 +1,35 @@
 package pacman.game;
 
+import fri.shapesge.Manager;
+
 public class Game {
+
+    public Game() {
+        Manager manager = new Manager();
+        manager.manageObject(this);
+    }
+
+    public void up() {
+        System.out.println("up event triggered");
+    }
+
+    public void down() {
+        System.out.println("down event triggered");
+    }
+
+    public void left() {
+        System.out.println("left event triggered");
+    }
+
+    public void right() {
+        System.out.println("right event triggered");
+    }
+
+    public void escape() {
+        System.out.println("escape event triggered");
+    }
+
+    public void tick() {
+        System.out.println("tick event triggered");
+    }
 }
