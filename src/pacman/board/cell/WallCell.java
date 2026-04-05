57f1cb514e6a3c6450ff4739b0c737d970da5066
Peter Magdík <petermagdik@gmail.com>
2026-04-05 12:35:21 +0200

    feat: implement WallCell

--- src/pacman/board/cell/WallCell.java
+++ src/pacman/board/cell/WallCell.java
@@ -1,4 +1,37 @@
 package pacman.board.cell;
 
-public class WallCell {
+import fri.shapesge.Square;
+import pacman.util.Position;
+import pacman.util.ScoreManager;
+
+public class WallCell extends Cell {
+    private final Square sprite;
+
+    public WallCell(Position position) {
+        super(position);
+        this.sprite = new Square();
+        this.sprite.changeColor("black");
+        this.sprite.changePosition(this.getCol() * SIZE, this.getRow() * SIZE);
+        this.sprite.changeSize(SIZE);
+    }
+
+    @Override
+    public void draw() {
+        this.sprite.makeVisible();
+    }
+
+    @Override
+    public void hide() {
+        this.sprite.makeInvisible();
+    }
+
+    @Override
+    public void onEnter(ScoreManager scoreManager) {
+        throw new IllegalStateException("can't enter wall");
+    }
+
+    @Override
+    public boolean isWalkable() {
+        return false;
+    }
 }
